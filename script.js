5ca1cef137ece01eade578bfbb357fddea889685
Reyhaneh <reyhaneh.zhs@gmail.com>
2025-04-10 18:37:26 +0330

    Setting the score elements to 0 for both players.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'Use strict';
+
+// Selecting elements
+const score0EL = document.getElementById('score--0');
+const score1EL = document.getElementById('score--1');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;

f32cd62df8c5f147a99db20eafd5d8a08416c8b3
Reyhaneh <reyhaneh.zhs@gmail.com>
2025-04-10 19:13:42 +0330

    Rolling dice functionality

--- script.js
+++ script.js
@@ -4,8 +4,22 @@
 const score0EL = document.getElementById('score--0');
 const score1EL = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `images/dice-${dice}.png`;
+  // 3. Check for rolled 1: if true, switch to next player
+});

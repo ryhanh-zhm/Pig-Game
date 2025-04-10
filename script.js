ca213123e914cfd66729f112e22771f48468b261
Reyhaneh <reyhaneh.zhs@gmail.com>
2025-04-10 18:48:44 +0330

    hiding the dice at starting condition.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0EL = document.getElementById('score--0');
 const score1EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
+// Starting conditions
 score0EL.textContent = 0;
 score1EL.textContent = 0;
+diceEl.classList.add('hidden');
